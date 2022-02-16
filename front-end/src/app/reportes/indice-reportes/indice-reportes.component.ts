d0eeb352dd55e65815bf7afcf8177c599ba74443
fullstackkmoreno <99702197+fullstackkmoreno@users.noreply.github.com>
2022-02-16 10:28:26 -0500

    Add files via upload

---
+++ front-end/src/app/reportes/indice-reportes/indice-reportes.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-indice-reportes',
+  templateUrl: './indice-reportes.component.html',
+  styleUrls: ['./indice-reportes.component.css']
+})
+export class IndiceReportesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
